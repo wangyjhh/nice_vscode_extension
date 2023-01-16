678343d62f999e587b37a66a86858f241db48422
hikaru <827394320@qq.com>
2023-01-16 10:54:47 +0800

    翻译接口测试通过

--- test/main.spec.ts
+++ test/main.spec.ts
@@ -1,5 +1,14 @@
 import { test, expect } from "vitest"
+import { translate } from "../src/utils/translate/translate"
 
 test("init", () => {
 	expect(true).toBe(true)
+})
+
+test("init", async () => {
+	const text = "hello"
+	const translateResult = (await translate(text)).data.translateResult[0][0].tgt
+	console.log(translateResult)
+
+	expect(translateResult).toBe("你好")
 })
