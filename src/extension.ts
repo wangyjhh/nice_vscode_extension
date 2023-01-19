236a99988da55bb70b8ad6380029df625f33f500
hikaru <827394320@qq.com>
2023-01-19 10:56:25 +0800

    更改插件名称

--- src/extension.ts
+++ src/extension.ts
@@ -19,6 +19,26 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 		vscode.window.showInformationMessage(translateResult)
 	})
+
+	let toUpperCaseCommand = vscode.commands.registerCommand("wyj.toUpperCaseCommand", async () => {
+		const editor = vscode.window.activeTextEditor
+		if (!editor) {
+			return
+		}
+		const selectionText = editor.document.getText(editor.selection)
+		if (!selectionText) {
+			vscode.window.showWarningMessage("请先选择文本后在执行该命令！")
+			return
+		}
+
+		const upperCaseResult = selectionText.toUpperCase()
+		if (!upperCaseResult) {
+			vscode.window.showErrorMessage("转换大写字母失败")
+			return
+		}
+		editor.document.getText(editor.selection)
+		editor.document
+	})
 }
 
 export function deactivate() {}
--- test/main.spec.ts
+++ test/main.spec.ts
@@ -5,10 +5,17 @@ test("init", () => {
 	expect(true).toBe(true)
 })
 
-test("init", async () => {
+test("translate", async () => {
 	const text = "hello"
 	const translateResult = (await translate(text)).data.translateResult[0][0].tgt
 	console.log(translateResult)
 
 	expect(translateResult).toBe("你好")
+})
+
+test("toUpperCase", async () => {
+	const text: string = "hello"
+	const upperCaseResult = text.toUpperCase()
+	console.log(upperCaseResult)
+	expect(upperCaseResult).toBe("HELLO")
 })
