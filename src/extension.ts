7e694f478d31dae55f57668a77b6e22475667e77
hikaru <827394320@qq.com>
2023-01-21 00:04:50 +0800

    项目基本运行正常无bug，发布0.1.0版本

--- src/extension.ts
+++ src/extension.ts
@@ -1,40 +1,40 @@
-import * as vscode from "vscode"
+import { ExtensionContext, commands, window } from "vscode"
 import { translate } from "./utils/translate/translate.js"
 import { theFirstLetterReverse, isUpperCase } from "./utils/string/string.js"
 
-export function activate(context: vscode.ExtensionContext) {
-	let translateCommand = vscode.commands.registerCommand("wyj.translationCommand", async () => {
-		const editor = vscode.window.activeTextEditor
+export function activate(context: ExtensionContext) {
+	let translateCommand = commands.registerCommand("wyj.translationCommand", async () => {
+		const editor = window.activeTextEditor
 		if (!editor) {
 			return
 		}
 		const selectionText = editor.document.getText(editor.selection)
 		if (!selectionText) {
-			vscode.window.showWarningMessage("请先选择文本后在执行该命令！")
+			window.showWarningMessage("请先选择文本后在执行该命令！")
 			return
 		}
 		const translateResult = (await translate(selectionText)).data.translateResult[0][0].tgt
 		if (!translateResult) {
-			vscode.window.showErrorMessage("翻译失败")
+			window.showErrorMessage("翻译失败")
 			return
 		}
-		vscode.window.showInformationMessage(translateResult)
+		window.showInformationMessage(translateResult)
 	})
 
-	let toUpperOrLowerCaseCommand = vscode.commands.registerCommand("wyj.toUpperOrLowerCaseCommand", async () => {
-		const editor = vscode.window.activeTextEditor
+	let toUpperOrLowerCaseCommand = commands.registerCommand("wyj.toUpperOrLowerCaseCommand", async () => {
+		const editor = window.activeTextEditor
 		if (!editor) {
 			return
 		}
 		const selectionText = editor.document.getText(editor.selection)
 		if (!selectionText) {
-			vscode.window.showWarningMessage("请先选择文本后在执行该命令！")
+			window.showWarningMessage("请先选择文本后在执行该命令！")
 			return
 		}
 
 		const result = isUpperCase(selectionText) ? selectionText.toLocaleLowerCase() : selectionText.toLocaleUpperCase()
 		if (!result) {
-			vscode.window.showErrorMessage("转换大小写失败")
+			window.showErrorMessage("转换大小写失败")
 			return
 		}
 
@@ -43,20 +43,20 @@ export function activate(context: vscode.ExtensionContext) {
 		})
 	})
 
-	let theFirstLetterReverseCommand = vscode.commands.registerCommand("wyj.theFirstLetterReverseCommand", async () => {
-		const editor = vscode.window.activeTextEditor
+	let theFirstLetterReverseCommand = commands.registerCommand("wyj.theFirstLetterReverseCommand", async () => {
+		const editor = window.activeTextEditor
 		if (!editor) {
 			return
 		}
 		const selectionText = editor.document.getText(editor.selection)
 		if (!selectionText) {
-			vscode.window.showWarningMessage("请先选择文本后在执行该命令！")
+			window.showWarningMessage("请先选择文本后在执行该命令！")
 			return
 		}
 
 		const result = theFirstLetterReverse(selectionText)
 		if (!result) {
-			vscode.window.showErrorMessage("首字母转换大小写失败")
+			window.showErrorMessage("首字母转换大小写失败")
 			return
 		}
 
@@ -65,5 +65,4 @@ export function activate(context: vscode.ExtensionContext) {
 		})
 	})
 }
-
 export function deactivate() {}
